4d7057b1473a7e956aa8908cfd42ad9b8946d241
Redbu11 <Redbu11dev777@gmail.com>
2021-06-14 15:23:57 +0700

    radio button icons done

--- app/src/main/java/com/vashkpi/digitalretailgroup/Application.kt
+++ app/src/main/java/com/vashkpi/digitalretailgroup/Application.kt
@@ -4,6 +4,17 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+//Icon conversion (55dp x 55dp)
+//dp	px	sp	mm	in	pt
+//ldpi	@ 55.00dp	= 41.25px	= 55.00sp	= 8.73mm	= 0.34in	= 24.75pt
+//mdpi	@ 55.00dp	= 55.00px	= 55.00sp	= 8.73mm	= 0.34in	= 24.75pt
+//tvdpi	@ 55.00dp	= 73.22px	= 55.00sp	= 8.73mm	= 0.34in	= 24.75pt
+//hdpi	@ 55.00dp	= 82.50px	= 55.00sp	= 8.73mm	= 0.34in	= 24.75pt
+//xhdpi	@ 55.00dp	= 110.00px	= 55.00sp	= 8.73mm	= 0.34in	= 24.75pt
+//xxhdpi	@ 55.00dp	= 165.00px	= 55.00sp	= 8.73mm	= 0.34in	= 24.75pt
+//xxxhdpi	@ 55.00dp	= 220.00px	= 55.00sp	= 8.73mm	= 0.34in	= 24.75pt
+
+
 @HiltAndroidApp
 class Application: Application() {
 
