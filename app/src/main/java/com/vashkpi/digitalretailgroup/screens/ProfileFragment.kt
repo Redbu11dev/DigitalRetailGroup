467dad9905b7115a86c2b4003138d1739f36dbbb
Redbu11 <Redbu11dev777@gmail.com>
2021-06-18 00:20:59 +0700

    experiments

--- app/src/main/java/com/vashkpi/digitalretailgroup/screens/ProfileFragment.kt
+++ app/src/main/java/com/vashkpi/digitalretailgroup/screens/ProfileFragment.kt
@@ -102,6 +102,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
         //TODO move date picker to a separate nav component fragment
         val builder = MaterialDatePicker.Builder.datePicker()
         val picker = builder.build()
+
 //        val outputDateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).apply {
 //            timeZone = TimeZone.getTimeZone("UTC")
 //        }
@@ -146,63 +147,6 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
     override fun observeViewModel() {
         super.observeViewModel()
 
-//        viewModel.name.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-//            .onEach {
-//                if (binding.firstNameText.text.toString() != it) {
-//                    Timber.d("firstNameText changed to: $it")
-//                    binding.firstNameText.setText(it)
-//                }
-//                else {
-//                    Timber.d("firstNameText changed observed, but was the same: $it")
-//                }
-//            }.launchIn(lifecycleScope)
-//
-//        viewModel.surname.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-//            .onEach {
-//                if (binding.surnameText.text.toString() != it) {
-//                    Timber.d("surname changed to: $it")
-//                    binding.surnameText.setText(it)
-//                }
-//                else {
-//                    Timber.d("surname changed observed, but was the same: $it")
-//                }
-//            }.launchIn(lifecycleScope)
-//
-//        viewModel.middleName.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-//            .onEach {
-//                if (binding.middleNameText.text.toString() != it) {
-//                    Timber.d("middleNameText changed to: $it")
-//                    binding.middleNameText.setText(it)
-//                }
-//                else {
-//                    Timber.d("middleNameText changed observed, but was the same: $it")
-//                }
-//            }.launchIn(lifecycleScope)
-//
-//        viewModel.birthDate.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-//            .onEach {
-//                if (binding.birthDateText.text.toString() != it) {
-//                    Timber.d("birthDateText changed to: $it")
-//                    binding.birthDateText.setText(it)
-//                }
-//                else {
-//                    Timber.d("birthDateText changed observed, but was the same: $it")
-//                }
-//            }.launchIn(lifecycleScope)
-//
-//        viewModel.genderRadioId.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-//            .onEach {
-//                if (binding.radioGroup.checkedRadioButtonId != it) {
-//                    Timber.d("checkedRadioButtonId changed to: $it")
-//                    binding.radioGroup.check(it)
-//                }
-//                else {
-//                    Timber.d("checkedRadioButtonId changed observed, but was the same: $it")
-//                }
-//            }.launchIn(lifecycleScope)
-
-        //
-
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             viewModel.name.collect {
                 if (binding.firstNameText.text.toString() != it) {
@@ -210,7 +154,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
                     binding.firstNameText.setText(it)
                 }
                 else {
-                    Timber.d("firstNameText changed observed, but was the same: $it")
+                    Timber.d("firstNameText change observed, but was the same: $it")
                 }
             }
         }
@@ -222,7 +166,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
                     binding.surnameText.setText(it)
                 }
                 else {
-                    Timber.d("surname changed observed, but was the same: $it")
+                    Timber.d("surname changed observe, but was the same: $it")
                 }
             }
         }
@@ -234,7 +178,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
                     binding.middleNameText.setText(it)
                 }
                 else {
-                    Timber.d("middleNameText changed observed, but was the same: $it")
+                    Timber.d("middleNameText changed observe, but was the same: $it")
                 }
             }
         }
@@ -246,7 +190,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
                     binding.birthDateText.setText(it)
                 }
                 else {
-                    Timber.d("birthDateText changed observed, but was the same: $it")
+                    Timber.d("birthDateText changed observe, but was the same: $it")
                 }
             }
         }
@@ -254,11 +198,11 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             viewModel.genderRadioId.collect {
                 if (binding.radioGroup.checkedRadioButtonId != it) {
-                    Timber.d("checkedRadioButtonId changed to: $it")
+                    Timber.d("checkedRadioButtonId change to: $it")
                     binding.radioGroup.check(it)
                 }
                 else {
-                    Timber.d("checkedRadioButtonId changed observed, but was the same: $it")
+                    Timber.d("checkedRadioButtonId change observed, but was the same: $it")
                 }
             }
         }
@@ -276,25 +220,6 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
                 }
             }
         }
-
-//        viewLifecycleOwner.addRepeatingJob(Lifecycle.State.STARTED) {
-//            viewModel.profileDataChanged.collect {
-//                Timber.d("profileDataChanged: $it")
-//                if (it) {
-//                    binding.saveBtn.visibility = View.VISIBLE
-//                }
-//                else {
-//                    binding.saveBtn.visibility = View.GONE
-//                }
-//            }
-//        }
-//
-//        viewLifecycleOwner.addRepeatingJob(Lifecycle.State.STARTED) {
-//            viewModel.resetViewsEvent.collect {
-//                Timber.d("collecting reset views event: $it")
-//                setViewsFromLocalValues()
-//            }
-//        }
 
     }
 
@@ -326,246 +251,3 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(F
     }
 
 }
-
-
-
-//package com.vashkpi.digitalretailgroup.screens
-//
-//import android.view.View
-//import androidx.core.widget.doAfterTextChanged
-//import androidx.core.widget.doOnTextChanged
-//import androidx.fragment.app.setFragmentResultListener
-//import androidx.fragment.app.viewModels
-//import androidx.lifecycle.Lifecycle
-//import androidx.lifecycle.addRepeatingJob
-//import androidx.lifecycle.lifecycleScope
-//import androidx.lifecycle.repeatOnLifecycle
-//import androidx.navigation.fragment.findNavController
-//import androidx.navigation.fragment.navArgs
-//import androidx.navigation.ui.setupWithNavController
-//import com.google.android.material.datepicker.MaterialDatePicker
-//import com.vashkpi.digitalretailgroup.AppConstants
-//import com.vashkpi.digitalretailgroup.R
-//import com.vashkpi.digitalretailgroup.data.local.DataStoreRepository
-//import com.vashkpi.digitalretailgroup.data.models.datastore.UserInfo
-//import com.vashkpi.digitalretailgroup.screens.base.BaseFragment
-//import com.vashkpi.digitalretailgroup.databinding.FragmentProfileBinding
-//import com.vashkpi.digitalretailgroup.screens.dialogs.SaveProfileDataDialogFragment
-//import com.vashkpi.digitalretailgroup.utils.safeNavigate
-//import dagger.hilt.android.AndroidEntryPoint
-//import kotlinx.coroutines.cancel
-//import kotlinx.coroutines.flow.collect
-//import kotlinx.coroutines.launch
-//import kotlinx.coroutines.runBlocking
-//import timber.log.Timber
-//import java.text.SimpleDateFormat
-//import java.util.*
-//import javax.inject.Inject
-//
-//@AndroidEntryPoint
-//class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>(FragmentProfileBinding::inflate) {
-//
-//    @Inject
-//    lateinit var dataStoreRepository: DataStoreRepository
-//
-//    override val viewModel: ProfileViewModel by viewModels()
-//
-//    private val args: ProfileFragmentArgs by navArgs()
-//
-//    override fun setUpViews() {
-//        super.setUpViews()
-//
-//        val toolbar = binding.customToolbar.toolbar
-//
-//        val navController = findNavController()
-//        toolbar.setupWithNavController(navController)
-//        //toolbar.setNavigationIcon(R.drawable.ic_bell)
-//        //toolbar.nav
-//
-//        val isRegistration = args.isRegistration
-//
-//        if (isRegistration) {
-//            binding.saveBtn.setText(R.string.profile_btn_1_save_data)
-//            binding.logoutBtn.setText(R.string.profile_btn_2_later)
-//            //findNavController().currentDestination?.label = getString(R.string.profile_title)
-//            toolbar.setTitle(R.string.profile_title_is_registration)
-//
-//        }
-//        else {
-//            binding.saveBtn.setText(R.string.profile_btn_1_save)
-//            binding.logoutBtn.setText(R.string.profile_btn_2_logout)
-//            toolbar.setTitle(R.string.profile_title)
-//        }
-//
-//        binding.saveBtn.setOnClickListener {
-//            if (isRegistration) {
-//                //save directly and navigate to barcode
-//                viewModel.saveProfileData(getUserInfoFromFields(), true)
-//            }
-//            else {
-//                //show dialog
-//                findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToSaveProfileDataDialogFragment(false))
-//            }
-//        }
-//
-//        binding.logoutBtn.setOnClickListener {
-//            if (isRegistration) {
-//                //show dialog
-//                findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToSaveProfileDataDialogFragment(true))
-//            }
-//            else {
-//                //log out
-//                //clear datastore and move to launcher fragment
-//                lifecycleScope.launch {
-//                    dataStoreRepository.clearDataStore()
-//                    viewModel.postNavigationEvent(ProfileFragmentDirections.actionGlobalLogoutToLauncher())
-//                }
-//            }
-//
-////            showMessage(
-////                R.string.snackbar_msg_message_removed,
-////                R.string.snackbar_btn_cancel,
-////                {
-////
-////                },
-////                2000)
-//        }
-//
-//        binding.surnameText.doAfterTextChanged {
-//            notifyProfileDataChanged()
-//        }
-//
-//        binding.firstNameText.doAfterTextChanged {
-//            notifyProfileDataChanged()
-//        }
-//
-//        binding.middleNameText.doAfterTextChanged {
-//            notifyProfileDataChanged()
-//        }
-//
-//        binding.birthDateText.doAfterTextChanged {
-//            notifyProfileDataChanged()
-//        }
-//
-//        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
-//            notifyProfileDataChanged()
-//        }
-//
-//        //TODO move date picker to a separate nav component fragment
-//        val builder = MaterialDatePicker.Builder.datePicker()
-//        val picker = builder.build()
-////        val outputDateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).apply {
-////            timeZone = TimeZone.getTimeZone("UTC")
-////        }
-//        val outputDateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault()).apply {
-//            timeZone = TimeZone.getTimeZone("UTC")
-//        }
-//
-//        picker.addOnPositiveButtonClickListener {
-//            binding.birthDateText.setText(outputDateFormat.format(it))
-//        }
-//
-//        binding.birthDateText.setOnClickListener {
-//            picker.show(parentFragmentManager, picker.toString())
-//        }
-//
-//        setViewsFromLocalValues()
-//
-//        //Listen for fragment results
-//        setFragmentResultListener(SaveProfileDataDialogFragment.REQUEST_KEY) { key, bundle ->
-//            // read from the bundle
-//            Timber.d("Received fragment result: $bundle")
-//            if (bundle["data"] == SaveProfileDataDialogFragment.RESULT_SAVE) {
-//                if (isRegistration) {
-//                    //as if "save" button was pressed
-//                    viewModel.saveProfileData(getUserInfoFromFields(), true)
-//                }
-//                else {
-//                    viewModel.saveProfileData(getUserInfoFromFields(), false)
-//                }
-//            }
-//            else if (bundle["data"] == SaveProfileDataDialogFragment.RESULT_DO_NOT_SAVE) {
-//                if (isRegistration) {
-//                    //navigate to barcode directly
-//                    binding.root.post {
-//                        viewModel.postNavigationEvent(ProfileFragmentDirections.actionProfileFragmentToNavigationBarcode())
-//                    }
-//                }
-//                else {
-//                    setViewsFromLocalValues()
-//                }
-//            }
-//        }
-//
-//    }
-//
-//    override fun observeViewModel() {
-//        super.observeViewModel()
-//
-//        viewLifecycleOwner.addRepeatingJob(Lifecycle.State.STARTED) {
-//            viewModel.profileDataChanged.collect {
-//                Timber.d("profileDataChanged: $it")
-//                if (it) {
-//                    binding.saveBtn.visibility = View.VISIBLE
-//                }
-//                else {
-//                    binding.saveBtn.visibility = View.GONE
-//                }
-//            }
-//        }
-//
-//        viewLifecycleOwner.addRepeatingJob(Lifecycle.State.STARTED) {
-//            viewModel.resetViewsEvent.collect {
-//                Timber.d("collecting reset views event: $it")
-//                setViewsFromLocalValues()
-//            }
-//        }
-//
-//    }
-//
-//    private fun setViewsFromLocalValues() {
-//        lifecycleScope.launch {
-//            dataStoreRepository.getUserInfo.collect {
-//                binding.surnameText.setText(it.surname)
-//                binding.firstNameText.setText(it.name)
-//                binding.middleNameText.setText(it.middle_name)
-//                binding.birthDateText.setText(it.date_of_birth)
-//                when (it.gender) {
-//                    AppConstants.GenderValues.FEMALE.value -> {
-//                        binding.radioGroup.check(R.id.radio_female)
-//                    }
-//                    else -> {
-//                        binding.radioGroup.check(R.id.radio_male)
-//                    }
-//                }
-//                this@launch.cancel()
-//            }
-//        }
-//    }
-//
-//    private fun getUserInfoFromFields(): UserInfo {
-//        val gender = when (binding.radioGroup.checkedRadioButtonId) {
-//            R.id.radio_male -> {
-//                AppConstants.GenderValues.MALE.value
-//            }
-//            else -> {
-//                AppConstants.GenderValues.FEMALE.value
-//            }
-//        }
-//
-//        return UserInfo(
-//            binding.firstNameText.text.toString(),
-//            binding.surnameText.text.toString(),
-//            binding.middleNameText.text.toString(),
-//            binding.birthDateText.text.toString(),
-//            gender
-//        )
-//    }
-//
-//    private fun notifyProfileDataChanged() {
-//        viewModel.profileDataChanged(
-//            getUserInfoFromFields()
-//        )
-//    }
-//
-//}
--- app/src/main/java/com/vashkpi/digitalretailgroup/screens/ProfileViewModel.kt
+++ app/src/main/java/com/vashkpi/digitalretailgroup/screens/ProfileViewModel.kt
@@ -11,7 +11,6 @@ import com.vashkpi.digitalretailgroup.data.models.outgoing.Accounts
 import com.vashkpi.digitalretailgroup.screens.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -24,11 +23,20 @@ class ProfileViewModel @Inject constructor(private val dataStoreRepository: Data
     //- compare it to values in the text fields
     //- if some of them do not match -> let the user save it (both locally and remote)
 
-    val name = MutableStateFlow("")
-    val surname = MutableStateFlow("")
-    val middleName = MutableStateFlow("")
-    val birthDate = MutableStateFlow("")
-    val genderRadioId = MutableStateFlow(-1)
+    private val _name = MutableStateFlow("")
+    val name: StateFlow<String> get() = _name
+
+    private val _surname = MutableStateFlow("")
+    val surname: StateFlow<String> get() = _surname
+
+    private val _middleName = MutableStateFlow("")
+    val middleName: StateFlow<String> get() = _middleName
+
+    private val _birthDate = MutableStateFlow("")
+    val birthDate: StateFlow<String> get() = _birthDate
+
+    private val _genderRadioId = MutableStateFlow(-1)
+    val genderRadioId: StateFlow<Int> get() = _genderRadioId
 
     private lateinit var _localUserInfo: UserInfo
 
@@ -61,11 +69,11 @@ class ProfileViewModel @Inject constructor(private val dataStoreRepository: Data
             }
         }
 
-        name.value = nameToSet
-        surname.value = surnameToSet
-        middleName.value = middleNameToSet
-        birthDate.value = birthDateToSet
-        genderRadioId.value = genderRadioIdToSet
+        _name.value = nameToSet
+        _surname.value = surnameToSet
+        _middleName.value = middleNameToSet
+        _birthDate.value = birthDateToSet
+        _genderRadioId.value = genderRadioIdToSet
 
     }
 
@@ -88,29 +96,24 @@ class ProfileViewModel @Inject constructor(private val dataStoreRepository: Data
     fun profileDataChanged(actualUserInfo: UserInfo) {
         //val generatedUserInfo = UserInfo(name, surname, middleName, birthDate, gender)
 
-        name.value = actualUserInfo.name
-        surname.value = actualUserInfo.surname
-        middleName.value = actualUserInfo.middle_name
-        birthDate.value = actualUserInfo.date_of_birth
+        _name.value = actualUserInfo.name
+        _surname.value = actualUserInfo.surname
+        _middleName.value = actualUserInfo.middle_name
+        _birthDate.value = actualUserInfo.date_of_birth
         when (actualUserInfo.gender) {
             AppConstants.GenderValues.FEMALE.value -> {
-                genderRadioId.value = R.id.radio_female
+                _genderRadioId.value = R.id.radio_female
             }
             AppConstants.GenderValues.MALE.value -> {
-                genderRadioId.value = R.id.radio_male
+                _genderRadioId.value = R.id.radio_male
             }
             else -> {
-                genderRadioId.value = -1
+                _genderRadioId.value = -1
             }
         }
 
         //compare to local
-        viewModelScope.launch {
-            dataStoreRepository.getUserInfo.collect { localUserInfo ->
-                compareLocalValuesToActual(localUserInfo, actualUserInfo)
-                this@launch.cancel()
-            }
-        }
+        compareLocalValuesToActual(_localUserInfo, actualUserInfo)
     }
 
 
@@ -171,89 +174,5 @@ class ProfileViewModel @Inject constructor(private val dataStoreRepository: Data
             cancel()
         }
     }
-
-
-//    private val _resetViewsEvent = MutableSharedFlow<Boolean>(replay = 0)
-//    val resetViewsEvent: SharedFlow<Boolean> = _resetViewsEvent
-//
-//    fun postResetViewsEvent() {
-//        viewModelScope.launch {
-//            _resetViewsEvent.emit(true)
-//            cancel()
-//        }
-//    }
-//
-//    private val _profileDataChanged = MutableStateFlow<Boolean>(false)
-//    val profileDataChanged: StateFlow<Boolean> = _profileDataChanged
-//
-//    fun profileDataChanged(generatedUserInfo: UserInfo) {
-//        //val generatedUserInfo = UserInfo(name, surname, middleName, birthDate, gender)
-//
-//        //compare to local
-//        viewModelScope.launch {
-//            dataStoreRepository.getUserInfo.collect { localUserInfo ->
-//                Timber.d("local: ${localUserInfo}")
-//                Timber.d("generated: ${generatedUserInfo}")
-//                if (localUserInfo == generatedUserInfo) {
-//                    Timber.d("UserInfo is equal")
-//                    //do nothing
-//                    _profileDataChanged.value = false
-//                }
-//                else {
-//                    Timber.d("UserInfo is not equal")
-//                    //let the user save it
-//                    _profileDataChanged.value = true
-//                }
-//                this@launch.cancel()
-//            }
-//        }
-//    }
-//
-//    fun saveProfileData(userInfo: UserInfo, isRegistration: Boolean) {
-//        viewModelScope.launch {
-//            dataStoreRepository.getUserId().collect { userId ->
-//                apiRepository.saveProfileInfo(Accounts(userId, userInfo)).collect {
-//                    when (it) {
-//                        is Resource.Loading -> {
-//                            //TODO()
-//                            Timber.i("it's loading")
-//                            postProgressViewVisibility(true)
-//                        }
-//                        is Resource.Error -> {
-//                            //TODO()
-//                            this@launch.cancel()
-//                            val message = it.error?.message
-//                            Timber.i("it's error: ${message}")
-//                            //it.error.
-//                            postProgressViewVisibility(false)
-//                            postNavigationEvent(ProfileFragmentDirections.actionGlobalMessageDialog(title = R.string.dialog_error_title, message = message.toString()))
-//                        }
-//                        is Resource.Success -> {
-//                            //TODO()
-//                            Timber.i("it's success")
-//                            //check if empty?!
-//                            it.data?.let { data ->
-//                                Timber.i("here is the data: $data")
-//
-//                                dataStoreRepository.saveUserInfo(userInfo)
-//
-//                                postProgressViewVisibility(false)
-//                                if (isRegistration) {
-//                                    postNavigationEvent(ProfileFragmentDirections.actionProfileFragmentToNavigationBarcode())
-//                                }
-//                                else {
-//                                    //clear the views
-//                                    postResetViewsEvent()
-//                                }
-//                                this@launch.cancel()
-//                            } ?: kotlin.run {
-//                                this@launch.cancel()
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
 
 }
