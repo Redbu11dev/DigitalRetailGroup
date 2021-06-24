ee27ed71c7f367b5d821143fd2e8bfcb6a96b3a0
Redbu11 <Redbu11dev777@gmail.com>
2021-06-24 15:27:46 +0700

    formatting date

--- app/src/main/java/com/vashkpi/digitalretailgroup/data/models/network/UserInfoDto.kt
+++ app/src/main/java/com/vashkpi/digitalretailgroup/data/models/network/UserInfoDto.kt
@@ -9,14 +9,24 @@ fun UserInfoDto.asDomainModel(): UserInfo {
         surname,
         middle_name,
         date_of_birth,
-        gender
+        gender.convertGenderToDomainFormat()
     )
+}
+
+fun String.convertGenderToDomainFormat(): String {
+    val original = this
+    val year = original.subSequence(0,3)
+    val month = original.subSequence(5,6)
+    val day = original.subSequence(8,9)
+
+
+    return "$day.$month.$year"
 }
 
 data class UserInfoDto(
     val name: String,
     val surname: String,
     val middle_name: String,
-    val date_of_birth: String, //"1111.11.30"
-    val gender: String, //по-русски "мужской"/"женский"
+    val date_of_birth: String, //formats: local - "30.11.1111" | server - "1111-11-30"
+    val gender: String, //по-русски "Мужской"/"Женский"
 )
